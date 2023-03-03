540d2fbcc3f4f4c55fcb17651b88872ece004aef
Ros Udom <pothvotha@gmail.com>
2023-03-03 17:35:17 +0700

    edit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,7 +6,7 @@
     </div>
 
     <div class="col-md-2">
-      <button @click="addItem" class="btn btn-success">add item</button>
+      <button @click="addItem" class="btn btn-success">Add Item</button>
     </div>
   </div>
   <table class="table">
@@ -20,11 +20,16 @@
     </thead>
     <tbody>
       <tr v-for="(task, index) in tasks" :key="index">
-        <th scope="row">{{ index + 1 }}</th>
+        <th scope="row">{{ index + 1 }} ./</th>
         <td class="text-center" :class="statusChecking(task.status)">{{ task.title }}</td>
         <td class="text-center">{{ task.status }}</td>
-        <td class="text-danger text-center">
-          <i class="bi bi-trash" @click="deleteItem(index)"></i>
+        <td class="text-center">
+          <span class="text-danger" style="cursor: pointer" @click="deleteItem(index)"
+            ><i class="bi bi-trash"></i>
+          </span>
+          <span class="text-primary" style="cursor: pointer" @click="editItem(index)"
+            ><i class="bi bi-pencil"></i>
+          </span>
         </td>
       </tr>
     </tbody>
@@ -38,54 +43,59 @@ export default {
     return {
       title: "To do list",
       itemInput: "",
-      tasks: [
-        {
-          title: "go home",
-          status: "to-do"
-        },
-        {
-          title: "eat chocolate 1 kilogram in 1 hour",
-          status: "finished"
-        },
-        {
-          title: "go home",
-          status: "to-do"
-        },
-        {
-          title: "go home",
-          status: "finished"
-        },
-        {
-          title: "go home",
-          status: "to-do"
-        },
-      ],
+      editTask: null,
+      changeStatus : ["todo","inprogress","finished"],
+      tasks: [{
+        title : "dhwudodw",
+        status : "todo"
+      }],
     }
   },
   methods: {
     addItem() {
-      if(this.itemInput.length === 0){
+      if (this.itemInput.length === 0) {
         return alert("Please input");
       }
-      this.tasks.push(
-        {title:this.title,status:this.status}
-      );
+      if(this.editTask === null){
+          this.tasks.push({
+            title:this.itemInput,
+            status: "to do"
+          });
+      }
+      else{
+        console.log(this.itemInput);
+        console.log(this.tasks[this.editTask].title);
+        this.tasks[this.editTask].title = this.itemInput;
+        this.editTask = null;
+      }
+
       this.itemInput = "";
     },
-    deleteItem(index){
-      this.tasks.splice(index,1);
+    deleteItem(index) {
+      this.tasks.splice(index, 1);
     },
-    statusChecking(_status){
-      return{
-        'finished' : _status === 'finished'
+
+    statusChecking(_status) {
+      return {
+        'finished': _status  === 'finished'
       }
     },
+    editItem(index){
+      this.itemInput = this.tasks[index].title;
+      this.editTask = index;
+    }
   }
 
 }
 </script>
 
 <style scoped>
+th {
+  font-weight: bold;
+  text-transform: uppercase;
+  font-family: sans-serif;
+}
+
 .finished {
   text-decoration: line-through;
 }
